5e3f184a0851943addd15bd4de89a6c14b971a06
Sam @ SimpliWorx <sam@simpliworx.com>
2025-04-21 02:42:34 -0500

    Reset supabase auth to ensure login redirects occur

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,7 +33,7 @@ export default defineNuxtConfig({
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_ANON_KEY,
     serviceKey: process.env.SUPABASE_SERVICE_KEY,
-    redirect: false,
+    redirect: true,
     redirectOptions: {
       login: '/login',
       callback: '/confirm',
